194aa325999c7621a7f4ab9db3b03d1fff6a0b47
Bala <balasubramanian219@gmail.com>
2020-04-07 17:36:06 +0200

    Database setup

---
+++ data/tpch/skinner.load.sql
@@ -0,0 +1,9 @@
+load lineitem /home/bala/projects/data/tpch/lineitem.tbl | NULL;
+load nation /home/bala/projects/data/tpch/nation.tbl | NULL;
+load part /home/bala/projects/data/tpch/part.tbl | NULL;
+load partsupp /home/bala/projects/data/tpch/partsupp.tbl | NULL;
+load supplier /home/bala/projects/data/tpch/supplier.tbl | NULL;
+load region /home/bala/projects/data/tpch/region.tbl | NULL;
+load orders /home/bala/projects/data/tpch/orders.tbl | NULL;
+load customer /home/bala/projects/data/tpch/customer.tbl | NULL;
+compress;
