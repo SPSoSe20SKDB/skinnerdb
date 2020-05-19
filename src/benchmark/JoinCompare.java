56e33562a30205a884dd5d836693dabe0969a8c1
jonaspietsch <info@jonaspietsch.de>
2020-05-19 15:06:19 +0200

    Vergleichsmöglichkeit hinzugefügt

--- src/benchmark/JoinCompare.java
+++ src/benchmark/JoinCompare.java
@@ -9,12 +9,24 @@ import java.nio.file.Path;
 import java.util.HashMap;
 
 public class JoinCompare {
+    public static int yes = 0;
+    public static int no = 0;
+    public static int sum = 0;
 
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
             System.out.println("Specify Skinner DB dir, " + "query directory");
             return;
         }
+        /*
+        if(sum == 0) {
+            String SkinnerDbDir = args[0];
+            PathUtil.initSchemaPaths(SkinnerDbDir);
+            CatalogManager.loadDB(PathUtil.schemaPath);
+            PathUtil.initDataPaths(CatalogManager.currentDB);
+            BufferManager.loadDB();
+        }
+        */
 
         Path resultsPath = new File("skinnerResults.txt").toPath();
 
@@ -28,13 +40,26 @@ public class JoinCompare {
         BenchMarkSkinner.main(args);
         String[] result2 = Files.readAllLines(resultsPath).toArray(new String[]{});
 
+        BufferManager.colToIndex.clear();
+
         boolean arePermutations = arePermutations(result1, result2);
 
+        sum++;
+
         if (arePermutations) {
-            System.out.println("All Same");
+            yes++;
+            System.out.println("All Same: " + yes + " times");
         } else {
-            System.out.println("Not Same");
+            no++;
+            System.out.println("Not Same: " + no + " times");
         }
+        if (sum == 10) {
+            System.out.println("End");
+            System.out.println("Yes: " + yes);
+            System.out.println("No: " + no);
+            return;
+        }
+        main(args);
     }
 
     static boolean arePermutations(String[] arr1, String[] arr2) {
